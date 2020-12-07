96ccc077be2a05aa3745a7769f87d3f4f51d4ef5
Maureen972 <maureen.delyon@efrei.net>
2020-12-07 12:55:58 -0400

    rendu

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,20 +1,26 @@
-import scala.collection.MapView
-import scala.io.BufferedSource
+import com.TP_MR.CaseClass.Country
+import scala.collection.mutable.MutableList
+import scala.io.{BufferedSource, Source}
+
 
 object Main extends App {
 
 override def main ( args: Array [String] ): Unit = {
-  val Airportcsv = io.Source.fromFile("/src/main/scala/source/Airport.csv")
-  val lst = read (lines, 0, MutableList(Country("id", "code", "continent","wikipedia_link","keywords")))
+  //val country = readC( C,0, MutableList(Country("id", "code", "continent", "wikipedia_link", "keywords")))
+  val Countrycsv = io.Source.fromFile("/src/main/scala/source/Country.csv")
+
+  val C = Countrycsv.getLines.toList
+
+
 }
 
-def readc(l: List[String],x: Int, xs: MutableList[Country]): MutableList[Country] = {
+  def readC(l: List[String],x: Int, xs: MutableList[Country]): MutableList[Country] = {
   if (l.isEmpty) return null
   else if (x < l.size){
-    val cols = l.apply(x).split(",").map(_.trim)
-    val s = l.size
-    val c = Country(cols(0), cols(1), cols(2), cols(3), cols(4))
-    xs += (Country(cols(0), cols(1), cols(2), cols(3), cols(4)))
+    val columns = l.apply(x).split(",").map(_.trim)
+    val size = l.size
+    val c = Country(columns(0), columns(1), columns(2), columns(3), columns(4))
+    xs += (Country(columns(0), columns(1), columns(2), columns(3), columns(4)))
     println(c)
     val y = x +1
     read(l,y,xs)
